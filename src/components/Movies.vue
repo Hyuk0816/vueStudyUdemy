85e9510bc720bd736e7c5b7992f743cea434672f
Hyuk <rlawogur816@gmail.com>
2024-08-27 21:58:26 +0900

    1-16강 구현 중

---
+++ src/components/Movies.vue
@@ -0,0 +1,37 @@
+<script>
+  export default {
+    name: 'MoviesComponent',
+    props: {
+      data: Array,
+    }
+  }
+</script>
+
+<template>
+  <h1>영화정보</h1>
+    <div class="container">
+      <div v-for="(movie, i) in data" :key="i" class="item">
+        <figure>
+          <img :src="`${movie.imgUrl}`" :alt="movie.title">
+        </figure>
+        <div class="info">
+          <h3 class="bg-yellow" :style="movie.textColor">
+            {{movie.title}}
+          </h3>
+          <p>개봉: {{movie.year}} </p>
+          <P>장르: {{movie.category}}</P>
+          <button @:click="$emit('increaseLikes', i)">좋아요</button>
+          <span>{{movie.like}}</span>
+          <p>
+            <button @:click="$emit('openModal', i)">상세보기</button>
+          </p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style>
+  .container {
+    padding: 20px;
+  }
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div class="search-box">
+    <input type="search"
+           v-model="inputText"
+           placeholder="검색어 입력"
+    >
+    <button>검색</button>
+  </div>
+  <p>{{inputText}}</p>
+</template>
+
+<script >
+export default {
+  name: "SearchBarComponent",
+  data(){
+    return {
+      inputText: '',
+    }
+  },
+  watch:{
+    inputText(name){
+      //입력한 영화 제목이 데이터에 있는지 확이하는 일
+
+    }
+  }
+}
+</script>
+
+<style>
+  .search-box{
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+  }
+
+  .search-box input{
+    padding: 5px 10px;
+  }
+
+  .search-box button{
+    margin: 0 0 0 10px;
+  }
+</style>
